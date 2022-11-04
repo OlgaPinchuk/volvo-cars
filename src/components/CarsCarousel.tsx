ca1d750348052450a1e14953fac20bb12588fccd
Olga Pinchuk <pinchukolya@gmail.com>
2022-11-04 18:41:11 +0100

    fix

--- src/components/CarsCarousel.tsx
+++ src/components/CarsCarousel.tsx
@@ -43,7 +43,7 @@ export const CarsCarousel: FC<iProps> = ({ items }: iProps) => {
     <Block
       extend={{
         maxWidth: "80vw",
-        margin: "0 13vw",
+        margin: "5vw 13vw",
         width: "max-content",
         overflow: "hidden",
       }}
